939cb9cd017cbf79f14ed024115395fd5a9084e1
/ <your_email@abc.com>
2023-10-05 17:01:34 +0100

    dfjkdj

--- install.sh
+++ install.sh
@@ -35,6 +35,6 @@ bash $HOME/brave.sh
 # Cleanup
 sudo apt purge -y nano
 
-sudo apt install -y xorg xclip build-essential git python3-pip i3 network-manager pcmanfm pulseaudio feh dunst unzip mpv curl flameshot keepassxc tmux wget ninja-build gettext cmake zathura parallel ffmpeg picom
+sudo apt update -y && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y && sudo apt autopurge -y
 
 printf "\e[1;32mYou reboot\n"
