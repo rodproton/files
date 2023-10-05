462b90b33c15470e093ddcf9cd310a94c20bb3eb
/ <your_email@abc.com>
2023-10-05 17:10:47 +0100

    fjdk

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ sudo sed -i -e 's/bookworm/testing/g' /etc/apt/sources.list
 sudo apt update -y && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y && sudo apt autopurge -y
 
 # Programs and Packages
-sudo apt install -y xorg xclip build-essential git python3-pip i3 network-manager pcmanfm pulseaudio feh dunst unzip mpv curl flameshot keepassxc tmux wget ninja-build gettext cmake zathura parallel ffmpeg compton
+sudo apt install -y xorg xclip build-essential git python3-pip i3 network-manager pcmanfm pulseaudio feh dunst unzip mpv curl flameshot keepassxc tmux wget ninja-build gettext cmake zathura parallel ffmpeg compton alacritty
 
 # Files
 bash $HOME/files.sh
