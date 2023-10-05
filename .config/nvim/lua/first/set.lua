713295f3a9b920c81def658f25333f42fed483ce
/ <your_email@abc.com>
2023-10-05 15:33:27 +0100

    fdj

--- .config/nvim/lua/first/set.lua
+++ .config/nvim/lua/first/set.lua
@@ -15,8 +15,8 @@ vim.opt.wrap = false
 -- Extend undo buffer
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
+--vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 -- Highlighted word are not persistent 
 vim.opt.hlsearch = false
 -- Highlighting is incremental
