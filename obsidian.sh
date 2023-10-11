11882881099325ea86b93bd8379045ea9b7881ff
../.. <your_email@abc.com>
2023-10-11 14:53:54 +0100

    fjkdj

--- obsidian.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget -O package.deb https://github.com/obsidianmd/obsidian-releases/releases/download/v1.4.14/obsidian_1.4.14_amd64.deb
-
-sudo apt install -y ./package.deb
-
-rm package.deb
