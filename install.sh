25d6fdc017ef841e4ea3927aed5873c28f7f52b7
../.. <your_email@abc.com>
2023-10-11 15:00:33 +0100

    kdfjk

--- install.sh
+++ install.sh
@@ -30,9 +30,6 @@ if [ -z $DISPLAY ] && [ $(tty) = /dev/tty1 ]; then
     exec startx &>/dev/null
 fi
 
-# Load Xresources
-xrdb $HOME/.Xresources
-
 #Keyboard Remap
 $HOME/.local/bin/remap-keys
 
@@ -127,13 +124,10 @@ if ! shopt -oq posix; then
   fi
 fi
 
-#Cycle auto-complete
-bind 'TAB:menu-complete'
-
-#Enable vi mode
+#Enable vi-mode
 set -o vi
-bind -m vi-command ' Control-l: clear-screen'
-bind -m vi-insert ' Control-l: clear-screen'
+bind -m vi-command 'Control-l: clear-screen'
+bind -m vi-insert 'Control-l: clear-screen'
 EOF
 }
 
